8ed671fe20e0801a38a8b9530d6e782bfd1016f8
Luis Amez <luis.amez.vitoria@gmail.com>
2018-11-05 15:07:26 +0000

    Add method reviewCount that returns the number of reviews the hotel has

--- models/hotel.js
+++ models/hotel.js
@@ -4,6 +4,14 @@ class Hotel {
     this.city = city;
     this.reviews = [];
   }
+
+  /**
+   * Return how many reviews the hotel has
+   * @return {number} - The number of reviews for the hotel
+   */
+  reviewCount() {
+    return this.reviews.length;
+  }
 }
 
 module.exports = Hotel;
