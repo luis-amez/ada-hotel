31a7a4fa90eeb7c8a7ae881833fb302aa7f43e8a
Luis Amez <luis.amez.vitoria@gmail.com>
2018-11-05 14:59:55 +0000

    Add test for review counting and rating methods when the hotel doesn't have reviews yet

--- test/hotel.test.js
+++ test/hotel.test.js
@@ -11,4 +11,12 @@ describe('Hotel', function() {
     expect(hotel.city).to.equal('London');
     expect(hotel.reviews).to.deep.equal([]);
   });
+
+  it('should return 0 and empty string if there are no reviews', () => {
+    const hotel = new Hotel("Hilton Metropole", "London");
+
+    expect(hotel.reviewCount()).to.equal(0);
+    expect(hotel.rating()).to.equal(0);
+    expect(hotel.ratingAsStars()).to.equal('');
+  });
 });
