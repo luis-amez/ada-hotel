505d1e17f15a38679cd6511c3de9db272fcfc07d
Luis Amez <luis.amez.vitoria@gmail.com>
2018-11-09 10:28:09 +0000

    Add error handler

--- server.js
+++ server.js
@@ -5,7 +5,20 @@ const app = express();
 
 app.use(formidable());
 
-// Routes and controllers, all together, :-O
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  res.status(404).json({code: 'not found'});
+});
+
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500).json({code: 'unexpected'});
+  }
+});
 
 app.listen(3000, function() {
   console.log('Server is listening on port 3000. Ready to accept requests!')
