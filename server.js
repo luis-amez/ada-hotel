6d126762571678cdf9d03f13dc4c4d893e6bf581
Luis Amez <luis.amez.vitoria@gmail.com>
2018-11-09 11:08:25 +0000

    Add POST /hotels route that saves a new hotel into the collection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const formidable = require('express-formidable');
 const HotelCollection = require('./models/hotelCollection');
+const Hotel = require('./models/hotel');
 
 const app = express();
 
@@ -9,7 +10,19 @@ app.use(formidable());
 let hotelCollection = HotelCollection.load('./hotels-list.json');
 
 app.get('/hotels', (req, res, next) => {
-  res.status(200).json(hotelCollection);
+  res.status(200).json(hotelCollection.hotels);
+});
+
+app.post('/hotels', (req, res, next) => {
+  if(!req.fields.name || !req.fields.city) {
+    return next(new Error('Name and city required!'));
+  }
+
+  let hotel = new Hotel(req.fields.name, req.fields.city);
+  hotelCollection.addHotel(hotel);
+  hotelCollection.saveFile('./hotels-list.json');
+
+  res.status(201).json(hotel);
 });
 
 // catch 404 and forward to error handler
